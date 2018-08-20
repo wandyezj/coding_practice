94a866defd7f2efc4f599261fbacbdc1384ef96b
wandyezj <wandyezj@outlook.com>
2018-08-19 22:21:38 -0700

    properly ignore out

--- node_hello/out/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var HelloWorld_1 = require("./src/HelloWorld");
-console.log(process.argv);
-HelloWorld_1.HelloWorld();
